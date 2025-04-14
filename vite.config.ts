824d92750df4a09045420fb07dbf90bd60427e9f
Arthur Rump <hello@arthurrump.com>
2025-04-14 12:09:49 +0200

    Add base path in vite build

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 // https://vitejs.dev/config/
 export default defineConfig({
     clearScreen: false,
+    base: "/basis_cs_machine/",
     build: {
         rollupOptions: {
             input: {
